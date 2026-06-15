5e374ac5b06657f91d1f721f72d204e70caf3847
victort236711-bit <victort236711@gmail.com>
2026-06-15 15:59:21 -0400

    Add files via upload

---
+++ Bucles anidados.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    for (int i = 1; i <=10; i++) {
+        
+    for (int j = 1; j <= 5; j++) {
+            
+            cout << " \ni:" <<i<<"j:"<<j;
+            
+            
+        } 
+        cout<<"\n";
+    }
+    
+    return 0;
+}
+
